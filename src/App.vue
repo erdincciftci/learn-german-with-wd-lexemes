a45687d59c223e130f56cf11cc2a87ca505755e9
erdincciftci <18039404+ciftciworks@users.noreply.github.com>
2021-01-29 00:11:17 +0300

    feat: created page structure, added some reactive data with composition api

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,82 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Learn German with Wikidata Lexemes!" />
- {{example[1]}}
- <li v-for="a in example" :key="a.sense">
+<div class="wrapper">
+  <header class="header"><img style="width: 30px;" alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld class="wmf-wmui-color-green30" msg="Learn German with Wikidata Lexemes!" />
+  </header>
+ 
+  <aside class="aside aside-1"> {{example[12]}} </aside>
+
+
+   <article class="main">    
+      
+   <input
+        type="text"
+        placeholder="Type a new todo item"
+        class="new-task-input"
+        v-model="newNativeTranslition"
+        @keyup.enter="addTask"
+      />
+      <button class="new-task-button" @click="addTask">+ Add</button>
+  
+  </article>
+  
+  <aside class="aside aside-2"><p> In English: {{ newNativeTranslition }}</p> +  
+  
+  </aside>
+  <footer class="footer"> Test {{translatedList[0]}} </footer>
+</div>
+
+ <!-- <li v-for="a in example" :key="a.sense">
       {{ a.sense }}
-    </li>
+    </li> -->
+
 </template>
+
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import { v4 as uuid } from 'uuid'
 import json from '../public/sensewlemma.json'
+import { reactive, toRefs, computed} from 'vue'
 
 export default {
   name: "App",
   components: {
     HelloWorld
   },
- data() {
-    return {
-      example : json
-      
+
+  setup() {
+    const state = reactive({
+      example: json,
+      newNativeTranslition: '',
+      originalSenses: [],
+      translatedList: []
+    })
+
+   const germanSenses = computed(() => {
+      return state.originalSenses
+    })
+    
+    const addTask = () => {
+      state.translatedList.push({
+        id: uuid(),
+        complete: false,
+        edit: false,
+        label: state.newNativeTranslition
+      })
+      state.newNativeTranslition = ''
     }
-  },
- mounted() {
 
-  json.forEach(x => { console.log(x.lemma, x.sense); });
-
-              }
+  
+  return {
+      ...toRefs(state),
+      addTask,
+      germanSenses
+    }
+    }
 };
 </script>
+
 
 <style>
 #app {
@@ -39,4 +87,57 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.wrapper {
+  display: flex;  
+  flex-flow: row wrap;
+  font-weight: bold;
+  text-align: center;
+}
+
+.wrapper > * {
+  padding: 10px;
+  flex: 1 100%;
+}
+
+.header {
+  background: white;
+}
+
+.footer {
+  background: lightgreen;
+}
+
+.main {
+  text-align: left;
+  background: deepskyblue;
+}
+
+.aside-1 {
+  background: gold;
+}
+
+.aside-2 {
+  background: hotpink;
+}
+
+@media all and (min-width: 600px) {
+  .aside { flex: 1 0 0; }
+  
+}
+
+@media all and (min-width: 800px) {
+  .main    { flex: 1 0px; }
+  .aside-1 { order: 1; } 
+  .main    { order: 2; }
+  .aside-2 { order: 3; }
+  .footer  { order: 4; }
+}
+
+body {
+  padding: 2em; 
+}
+
+
 </style>
+
