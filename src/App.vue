c4397e3b4ecdbfd4a8468327b074fa0e678255c0
Erdinç Çiftçi <erci@c092.local>
2021-01-28 22:02:41 +0300

    first day end of data stuff, it can read json with senses and lexeme

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <HelloWorld msg="Learn German with Wikidata Lexemes!" />
+ {{example[1]}}
+ <li v-for="a in example" :key="a.sense">
+      {{ a.sense }}
+    </li>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import json from '../public/sensewlemma.json'
 
 export default {
   name: "App",
   components: {
     HelloWorld
-  }
+  },
+ data() {
+    return {
+      example : json
+      
+    }
+  },
+ mounted() {
+
+  json.forEach(x => { console.log(x.lemma, x.sense); });
+
+              }
 };
 </script>
 
